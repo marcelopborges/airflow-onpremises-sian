bc6d177adeb2980e713f3d12b3db9899bf059d91
marcelopborges <54992376+marcelopborges@users.noreply.github.com>
2024-06-19 09:10:12 -0300

    Corrigindo execution date

--- dags/pipeline_hp_dados_escala.py
+++ dags/pipeline_hp_dados_escala.py
@@ -185,7 +185,7 @@ def mark_end(**context):
 @dag(
     start_date=datetime(2024, 2, 26),
     schedule_interval='30 11 * * *',
-    catchup=False,
+    catchup=True,
     max_active_runs=1,
     tags=['airbyte', 'HP', 'Mix-Telematics']
 )
